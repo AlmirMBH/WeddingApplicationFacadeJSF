504175360562885f4e11c7ac89935975906e6bea
AlmirMBH <almir.mustafic.tz@gmail.com>
2020-07-28 01:59:08 +0200

    Wedding Application:
    1. JSF, Bootstrap, HTML, CSS, PrimeFaces
    2. FacesServlet, Views, ManagedBeans
    3. SessionBeans(Local), AbstractFacade, Model/Entity, JPA, glassfish 5, MyJDBS connector 8
    4. MySQL (Workbench)
    
    1. Sessions (following users through sessions)
    2. EJB
    3. EntityManager
    4. REGEX
    5. Privileges
    6. Pagination
    
    1. Login/Register
    2. Shopping Cart
    3. Product card view (photo, name, description, price, buy)
    4. Registered user shops
    5. Shop categories card view (photo, name, description, contact, address)
    6. Users view table
    7. Test credit card validation
    8. Admin view
    9. User view

--- WeddingApplicationFinalVersion/src/java/wedding/screen/ProductManagedBean.java
+++ WeddingApplicationFinalVersion/src/java/wedding/screen/ProductManagedBean.java
@@ -24,6 +24,7 @@ public class ProductManagedBean implements Serializable {
     private Integer quantity;
     private List<ShoppingCartItem> shoppingCartItems = new ArrayList<>();
     private Category category;
+    private Product product = new Product();
     
     public ProductManagedBean() {
     }
@@ -72,8 +73,46 @@ public class ProductManagedBean implements Serializable {
         return productFacadeLocal.getAllSuits();        
     }
     
-    
-    
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public List<Product> findAll() {
+        return productFacadeLocal.findAll();
+    }
+
+    public String add() {
+        this.productFacadeLocal.create(this.product);
+        this.clear();
+        return "index";
+    }
+
+    public void delete(Product product) {
+        this.productFacadeLocal.remove(product);
+    }
+
+    public String edit(Product product) {
+        this.product = product;
+        return "edit";
+    }
+
+    public String edit() {
+        this.productFacadeLocal.edit(this.product);
+        this.product = new Product();
+        return "index";
+    }
+
+    public void clear() {
+        this.product.setName(null);
+        this.product.setCode(null);
+        this.product.setColor(null);
+        this.product.setSize(null);
+        this.product.setPrice(0);
+    }
     
     
     
